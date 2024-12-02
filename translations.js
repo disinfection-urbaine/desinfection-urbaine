c71fe53ecc1ef0b763a386cf8cf43b49fbce3c9d
disinfection-urbaine <DesinfectionUrbaine@gmail.com>
2024-12-02 13:14:39 +0100

    Create translations.js

---
+++ translations.js
@@ -0,0 +1,17 @@
+const translations = {
+    it: {
+        welcome: "Benvenuti su Desinfection Urbaine",
+        contact: "Contattaci",
+        services: "I nostri servizi",
+    },
+    en: {
+        welcome: "Welcome to Desinfection Urbaine",
+        contact: "Contact Us",
+        services: "Our Services",
+    },
+    fr: {
+        welcome: "Bienvenue à Desinfection Urbaine",
+        contact: "Contactez-nous",
+        services: "Nos Services",
+    },
+};
